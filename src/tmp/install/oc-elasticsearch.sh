5f6b9e6a14c11ab739acf9b5ea7187a4c6093898
Joern Ott <joern.ott@ott-consult.de>
2017-11-16 13:24:42 +0000

    Choose Repo based on version

--- src/tmp/install/oc-elasticsearch.sh
+++ src/tmp/install/oc-elasticsearch.sh
@@ -4,7 +4,7 @@ set -x
 
 curl -sSo /tmp/install/functions.sh https://raw.githubusercontent.com/joernott/docker-oc-install-library/master/install_functions.sh
 source /tmp/install/functions.sh
-add_repos elasticsearch5
+add_repos elasticsearch${ELASTICSEARCH_VERSION:0:1}
 create_user_and_group
 install_software elasticsearch-${ELASTICSEARCH_VERSION}
 
