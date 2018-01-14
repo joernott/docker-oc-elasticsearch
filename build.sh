68f6f9ee7dd0203691a957a1ef850d885d872884
Jörn Ott <joern.ott@ott-consult.de>
2018-01-14 22:58:14 +0100

    Add label, track image history

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
+set -x
+curl -sSo src/tmp/install/functions.sh https://raw.githubusercontent.com/joernott/docker-oc-install-library/master/install_functions.sh
+source src/tmp/install/functions.sh
 
+patch_dockerfile Dockerfile5
 docker build -f Dockerfile5 -t registry.ott-consult.de/oc/elasticsearch:5 .
 docker push registry.ott-consult.de/oc/elasticsearch:5
+
+patch_dockerfile6
 docker build -f Dockerfile6 -t registry.ott-consult.de/oc/elasticsearch:6 .
 docker push registry.ott-consult.de/oc/elasticsearch:6
 docker tag registry.ott-consult.de/oc/elasticsearch:6 registry.ott-consult.de/oc/elasticsearch:latest
 docker push registry.ott-consult.de/oc/elasticsearch:6
 docker push registry.ott-consult.de/oc/elasticsearch:latest
-
