afa2bec0246f4de98177f063406c5ee9dab7b27c
Joern Ott <joern.ott@ott-consult.de>
2019-03-03 19:09:04 +0000

    Bump version

--- build.sh
+++ build.sh
@@ -14,3 +14,7 @@ docker push registry.ott-consult.de/oc/elasticsearch:6
 docker tag registry.ott-consult.de/oc/elasticsearch:6 registry.ott-consult.de/oc/elasticsearch:latest
 docker push registry.ott-consult.de/oc/elasticsearch:6
 docker push registry.ott-consult.de/oc/elasticsearch:latest
+
+patch_dockerfile Dockerfile7
+docker build -f Dockerfile7 -t registry.ott-consult.de/oc/elasticsearch:7 .
+docker push registry.ott-consult.de/oc/elasticsearch:7
